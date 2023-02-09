99eb80799156851d99d7d72017725d874ad2ec73
eloy <eloymartos73@gmail.com>
2023-02-09 16:53:06 +0100

    resolución del ticket 04

--- src/main/kotlin/Ataque.kt
+++ src/main/kotlin/Ataque.kt
@@ -1,2 +1,24 @@
-class Ataque {
+class Ataque(nombre:String, tipo:String, fuerza:Int) {
+
+    var nombre = ""
+
+    var tipo = ""
+
+    var fuerza = 0
+        get() = field
+        set(value) {
+            if (value<0) throw Exception("la has liao!!!!!11!!!!!!111!")
+            field = value
+        }
+
+    init {
+        this.nombre = nombre
+        this.tipo = tipo
+        this.fuerza = fuerza
+    }
+
+    override fun toString(): String {
+        return "Soy el mejor ataque de todos! $nombre, tipo $tipo, con $fuerza de fuerza! "
+    }
+
 }
