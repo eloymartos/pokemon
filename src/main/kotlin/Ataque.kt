95a540f19702661b812f30a06ca99437e806e9f9
eloy <eloymartos73@gmail.com>
2023-02-16 15:55:27 +0100

    corregir ataque, que había una funcion sin terminar e innecesaria

--- src/main/kotlin/Ataque.kt
+++ src/main/kotlin/Ataque.kt
@@ -21,8 +21,4 @@ class Ataque(nombre:String, tipo:String, fuerza:Int) {
         return "Soy el mejor ataque de todos! $nombre, tipo $tipo, con $fuerza de fuerza! "
     }
 
-    fun tipoAtaca(tipo_enemigo: String){
-        if (tipo in arrayOf("roca", "fuego"))
-    }
-
 }
