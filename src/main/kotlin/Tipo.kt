5e8fb64d2954186dbf731296d7d7f584b1de23a3
eloy <eloymartos73@gmail.com>
2023-02-16 16:11:56 +0100

    ticket AA0002, implementación de codigo para comprobar entrada de el atributo tipo

--- src/main/kotlin/Tipo.kt
+++ src/main/kotlin/Tipo.kt
@@ -1,4 +1,13 @@
 class Tipo(tipo:String) {
 
+    var tipo = tipo
+        set(value) {
+            if (value in arrayOf("acero", "agua", "bicho", "dragon", "electrico", "fantasma", "fuego",
+                    "hada", "hielo", "lucha", "normal", "planta", "psiquico", "roca", "siniestro", "tierra",
+                    "veneno", "volador")) field = value else {
+                        field = ""
+                        IllegalArgumentException("el tipo no es válido")
+                    }
+        }
 
 }
